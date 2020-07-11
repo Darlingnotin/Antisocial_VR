b9a0520566ee076300352118cf2ce19dd339f595
Darlingnotin <darlingnotin@hotmail.com>
2020-07-11 16:57:57 -0400

    Update DomainServer.cpp

--- domain-server/src/DomainServer.cpp
+++ domain-server/src/DomainServer.cpp
@@ -71,9 +71,9 @@ const QString DomainServer::REPLACEMENT_FILE_EXTENSION = ".replace";
 int const DomainServer::EXIT_CODE_REBOOT = 234923;
 
 #if USE_STABLE_GLOBAL_SERVICES
-const QString ICE_SERVER_DEFAULT_HOSTNAME = "ice.highfidelity.com";
+const QString ICE_SERVER_DEFAULT_HOSTNAME = "ice.darlingvr.net";
 #else
-const QString ICE_SERVER_DEFAULT_HOSTNAME = "dev-ice.highfidelity.com";
+const QString ICE_SERVER_DEFAULT_HOSTNAME = "ice.darlingvr.net";
 #endif
 
 QString DomainServer::_iceServerAddr { ICE_SERVER_DEFAULT_HOSTNAME };
