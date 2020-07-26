1722b15a1b0a90d045c2d2b2e3fe09b435c87744
Darlingnotin <darlingnotin@hotmail.com>
2020-07-26 03:35:23 -0400

    Update changeMetaverseApi.js

--- scripts/system/changeMetaverseApi.js
+++ scripts/system/changeMetaverseApi.js
@@ -1,18 +1,26 @@
 (function () {
     Menu.menuItemEvent.connect(onMenuItemEvent);
-    Menu.addMenu("Metaverse");
-    Menu.addMenuItem("Metaverse", "Subscribe to new metaverse");
+    Menu.addMenuItem("Settings", "Subscribe to new metaverse");
+    Menu.addMenuItem("Settings", "Return to original metaverse");
+    var MetaverseURLInSettings = Settings.getValue("metaverse/selectedMetaverseURL");
+    if (MetaverseURLInSettings == "") {
+        Settings.setValue("metaverse/selectedMetaverseURL", "http://metaverse.darlingvr.net:9400");
+    }
 
     function onMenuItemEvent(menuItem) {
         if (menuItem == "Subscribe to new metaverse") {
-            var metaverseUrl = Window.prompt("Enter metaverse URL", "http://metaverse.darlingvr.club:9400");
+            MetaverseURLInSettings = Settings.getValue("metaverse/selectedMetaverseURL");
+            var metaverseUrl = Window.prompt("Enter metaverse URL", MetaverseURLInSettings);
             if (metaverseUrl) {
                 Settings.setValue("metaverse/selectedMetaverseURL", metaverseUrl);
             }
+        } else if (menuItem == "Return to original metaverse") {
+            Settings.setValue("metaverse/selectedMetaverseURL", "http://metaverse.darlingvr.net:9400");
         }
     }
 
     Script.scriptEnding.connect(function () {
-        Menu.removeMenu("Metaverse");
+        Menu.removeMenuItem("Settings", "Subscribe to new metaverse");
+        Menu.removeMenuItem("Settings", "Return to original metaverse");
     });
 }());
